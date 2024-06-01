ddeb261c5e49374b5a12d2582d31f5cd43cd84b7
Hiuszz <laquanghieuvh@gmail.com>
2024-06-01 12:06:08 +0700

    update 12:05 1/6/2024

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -4,6 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('testtest', 'root', null, {
   host: 'localhost',
   dialect: 'mysql',
+  logging: false,
 });
 
 let connectDB = async() => {
