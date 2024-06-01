d189b0d735486974d69cf7426dec223d07424446
Hiuszz <laquanghieuvh@gmail.com>
2024-06-01 15:04:47 +0700

    update 3:04 PM 1/6/2024

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -5,9 +5,14 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'example@example.com',
+        email: 'admin@gmail.com',
+        password: '1112131415',
+        firstName: 'Lax',
+        lastName: 'Hius',
+        address: 'VietNam',
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole: 'R1',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
